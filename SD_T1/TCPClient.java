b1e0c0a2486d785aae1dc7531e81654ba99215cb
Lucas Alexandre Seemund <lucas9865@hotmail.com>
2023-04-07 17:24:48 -0300

    add: codigo base

---
+++ SD_T1/TCPClient.java
@@ -0,0 +1,111 @@
+package SD_T1;
+
+/**
+ * TCPClient: Cliente para conexao TCP
+ * Descricao: Envia uma informacao ao servidor e recebe confirmações ECHO
+ * Ao enviar "PARAR", a conexão é finalizada.
+ */
+
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.List;
+
+/* import para o sha512 */
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class TCPClient {
+	/* https://stackoverflow.com/a/33085670 */
+	public static String get_SHA_512_SecurePassword(String passwordToHash, String salt) {
+		String generatedPassword = null;
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-512");
+			md.update(salt.getBytes(StandardCharsets.UTF_8));
+			byte[] bytes = md.digest(passwordToHash.getBytes(StandardCharsets.UTF_8));
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < bytes.length; i++) {
+				sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+			}
+			generatedPassword = sb.toString();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return generatedPassword;
+	}
+
+	public static void main(String args[]) {
+		Socket clientSocket = null; // socket do cliente
+		Scanner reader = new Scanner(System.in); // ler mensagens via teclado
+
+		try {
+			/* Endereço e porta do servidor */
+			int serverPort = 6666;
+			InetAddress serverAddr = InetAddress.getByName("127.0.0.1");
+
+			/* conecta com o servidor */
+			clientSocket = new Socket(serverAddr, serverPort);
+
+			/* cria objetos de leitura e escrita */
+			DataInputStream in = new DataInputStream(clientSocket.getInputStream());
+			DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
+
+			String buffer = "";
+
+			System.out.print("Digite o usuario e a senha. (User, Password): ");
+			buffer = reader.nextLine();
+
+			/** separa o User do Passowrd */
+			List<String> bufferList;
+			String key;
+			/** separa por uma ',' ou apenas por espaço */
+			try{
+				bufferList = Arrays.asList(buffer.split(","));
+				key = bufferList.get(1); /** para verficar se tem , na get(1) */
+			}catch(ArrayIndexOutOfBoundsException exception){
+				bufferList = Arrays.asList(buffer.split(" "));
+			}
+			key = get_SHA_512_SecurePassword(bufferList.get(1).replace(" ", ""), "OnePiece");
+			out.writeUTF("CONNECT " + bufferList.get(0) + " " + key.replace(" ", ""));
+
+			buffer = in.readUTF();
+			if (buffer.equals("ERROR")) {
+				System.out.print("ERROR: senha errada");
+			}
+			boolean loop = false;
+			if (buffer.equals("SUCCESS")) {
+				System.out.println("Logado");
+				loop = true;
+			}
+
+			/* protocolo de comunicação */
+			while (loop) {
+				System.out.print("Mensagem: ");
+				buffer = reader.nextLine(); // lê mensagem via teclado
+
+				out.writeUTF(buffer); // envia a mensagem para o servidor
+
+				if (buffer.equals("PARAR"))
+					break;
+
+				buffer = in.readUTF(); // aguarda resposta do servidor
+				System.out.println("Server disse: " + buffer);
+			}
+		} catch (UnknownHostException ue) {
+			System.out.println("Socket:" + ue.getMessage());
+		} catch (EOFException eofe) {
+			System.out.println("EOF:" + eofe.getMessage());
+		} catch (IOException ioe) {
+			System.out.println("IO:" + ioe.getMessage());
+		} finally {
+			try {
+				clientSocket.close();
+			} catch (IOException ioe) {
+				System.out.println("IO: " + ioe);
+				;
+			}
+		}
+	} // main
+} // class
---
+++ SD_T1/TCPServer.java
@@ -0,0 +1,201 @@
+package SD_T1;
+
+/**
+ * TCPServer: Servidor para conexao TCP com Threads Descricao: Recebe uma
+ * conexao, cria uma thread, recebe uma mensagem e finaliza a conexao
+ */
+import java.net.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+public class TCPServer {
+    public static void main(String args[]) {
+        try {
+            int serverPort = 6666; // porta do servidor
+
+            /* cria um socket e mapeia a porta para aguardar conexao */
+            ServerSocket listenSocket = new ServerSocket(serverPort);
+
+            while (true) {
+                System.out.println("Servidor aguardando conexao ...");
+
+                /* aguarda conexoes */
+                Socket clientSocket = listenSocket.accept();
+
+                System.out.println("Cliente conectado ... Criando thread ...");
+
+                /* cria um thread para atender a conexao */
+                ClientThread c = new ClientThread(clientSocket);
+
+                /* inicializa a thread */
+                c.start();
+            } //while
+
+        }  catch (IOException e) {
+          System.out.println("Listen socket:" + e.getMessage());
+        } //catch
+    } //main
+} //class
+
+
+class Path {
+	String _path;
+	public Path(String str){
+		//this.path = new String(str);
+		this._path = (str);
+    }
+	public void addPath(String str){
+		//this.path = new String(this.path + str);
+		this._path = (this._path + str);
+	}
+	public String getPath(){
+		return this._path;
+	}
+}
+
+/**
+ * Classe ClientThread: Thread responsavel pela comunicacao
+ * Descricao: Rebebe um socket, cria os objetos de leitura e escrita,
+ * aguarda msgs clientes e responde com a msg + :OK
+ */
+class ClientThread extends Thread {
+    DataInputStream in;
+    DataOutputStream out;
+    Socket clientSocket;
+    Path _path;
+    FileInputStream fis = null;
+	String packageName = "SD_T1";
+
+    public ClientThread(Socket clientSocket) {
+        try {
+            this.clientSocket = clientSocket;
+            in = new DataInputStream(clientSocket.getInputStream());
+            out = new DataOutputStream(clientSocket.getOutputStream());
+            this._path = new Path("/");
+        } catch (IOException ioe) {
+            System.out.println("Connection:" + ioe.getMessage());
+        }
+    } //construtor
+
+    /* metodo executado ao iniciar a thread - start() */
+    @Override
+    public void run() {
+        try {
+            String buffer  = "";
+            boolean loop = true;
+            buffer = in.readUTF(); /* aguarda ( CONNECT user, password ) */
+
+            //verifica se o diretório do usuário existe.
+            File fileUser = new File(packageName + "/users");
+            if(!fileUser.exists()){
+                fileUser.mkdir();
+                //fileUser.close();
+            }
+
+            /* recebe o user e senha para validar */
+            /* separa o buffer por espaco */
+            FileWriter fileWriter;
+            //FileReader fileReader;
+            BufferedReader reader;
+            List<String> key = Arrays.asList(buffer.split(" "));
+			String userName = key.get(1).replace(" ", "");
+
+            /*System.out.println("Key[0]: " + key.get(0));
+            System.out.println("Key[1]: " + key.get(1));
+            System.out.println("Key[2]: " + key.get(2).replace(" ", ""));*/
+
+            if(key.get(0).equals("CONNECT")){
+                fileUser = new File( packageName + "/users/" + userName + ".txt" );
+                /* verifica a senha do user */
+                if(fileUser.exists()){
+                    //fileReader = new FileReader("users/" + userName + ".txt");
+                    //String line = fileReader.readLine();
+                    reader = new BufferedReader(new FileReader(packageName + "/users/" + userName + ".txt" ));
+                    String line = reader.readLine();
+                    System.out.println("leu o arquivo: " + line);
+                    reader.close();
+
+                    if(!(key.get(2).replace(" ", "").equals(line.toString()))){ /* usuario errou a senha, desconectando user */
+                        // log do servidor
+                        System.out.println("Error: User: " + userName + " Password Invalid.");
+
+                        // saida para para o cliente
+                        out.writeUTF("ERROR");
+                        in.close();
+              		    out.close();
+              		    clientSocket.close();
+                        loop = false;
+                    }
+                    else{ /* usuario conectado */
+                        out.writeUTF("SUCCESS");
+                        System.out.println("User: " + userName + " connected");
+                    }
+                }
+                else{ /* caso não tenha o user, cria um novo */
+                    System.out.println("Create User: " + userName);
+                    out.writeUTF("SUCCESS");
+                    System.out.println("User: " + userName + " connected");
+                    fileUser.createNewFile();
+                    fileWriter = new FileWriter(packageName + "/users/" + userName + ".txt");
+                    fileWriter.write(key.get(2));
+                    fileWriter.close();
+                }
+            }
+            else{ /* caso protocolo CONNECT não for encontrado desconecta o cliente */
+                out.writeUTF("ERROR");
+                System.out.println("Error: waiting for the CONNECT protocol");
+                in.close();
+                out.close();
+                clientSocket.close();
+                loop = false;
+            }
+
+            /* loop pimario */
+            while (loop) {
+                buffer = in.readUTF();   /* aguarda o envio de dados */
+
+                if(buffer == "PWD"){
+                    /* Devolve o caminho corrente (PATH) usando String UTF separando os diretórios por barra(/). */
+                    out.writeUTF(this._path.getPath());
+                    System.out.println("User: " + userName + " is at " + this._path.getPath());
+                }
+                else if(buffer == "CHDIR"){
+                    
+                }
+                else if(buffer == "GETFILES"){
+                    
+                }
+                else if(buffer == "GETDIRS"){
+                    
+                }
+                else if(buffer == "EXIT"){
+                    System.out.println("User: " + userName + " disconect.");
+                    in.close();
+                	out.close();
+                	clientSocket.close();
+                    break;
+                    }
+                else{
+                    System.out.println("Cliente disse: " + buffer);
+                    out.writeUTF("MSG: protocolo indevido");
+                }
+            }
+        } catch (EOFException eofe) {
+            System.out.println("EOF: " + eofe.getMessage());
+        } catch (IOException ioe) {
+            System.out.println("IOE: " + ioe.getMessage());
+        } finally {
+            try {
+                in.close();
+                out.close();
+                clientSocket.close();
+            } catch (IOException ioe) {
+                System.err.println("IOE: " + ioe);
+            }
+        }
+        System.out.println("Thread comunicação cliente finalizada.");
+    } //run
+} //class
